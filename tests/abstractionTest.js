678494f36832ed11489bd488c977c5f4bc186b97
unknown <obliviga@gmail.com>
2014-03-23 14:22:20 -0400

    first unit test. testing abstract factory

---
+++ tests/abstractionTest.js
@@ -0,0 +1,6 @@
+test( "Abstraction test", function Toyota(model) {
+	ok( this.model = model, "Toyota created!" );
+});
+test( "Abstraction test", function Lexus(model) {
+	ok( this.model = model, "Lexus created!" );
+});
