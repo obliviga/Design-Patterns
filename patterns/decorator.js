c7ae1d257fa4133c63bfe12bd7f57123d1e39a9d
unknown <obliviga@gmail.com>
2014-03-23 02:58:24 -0400

    added better decorator-constructor and tooltips

--- patterns/decorator.js
+++ patterns/decorator.js
@@ -1,44 +1,33 @@
-// A vehicle constructor
-function vehicle( vehicleType ){
- 
-    // some sane defaults
-    this.vehicleType = vehicleType || "car";
-    this.model = "default";
-    this.license = "00000-000";
- 
+// A person constructor
+function person(personRace) {
+
+	this.personRace = personRace || "human";
+	this.speed = 1;
+	this.language = "English";
+
 }
- 
-// Test instance for a basic vehicle
-var testInstance = new vehicle( "car" );
-console.log( testInstance );
- 
-// Outputs:
-// vehicle: car, model:default, license: 00000-000
- 
-// Lets create a new instance of vehicle, to be decorated
-var truck = new vehicle( "truck" );
- 
-// New functionality we're decorating vehicle with
-truck.setModel = function( modelName ){
-    this.model = modelName;
+
+// Create an instance of a regular person
+var Birth = new person("human");
+console.log(Birth);
+
+// Give birth to a super human, or decorated human
+var superHuman = new person("Super Human");
+
+// Super human's powers
+superHuman.setSpeed = function(speedValue) {
+	this.speed = speedValue;
 };
- 
-truck.setColor = function( color ){
-    this.color = color;
+
+superHuman.setPower = function(power) {
+	this.power = power;
 };
- 
-// Test the value setters and value assignment works correctly
-truck.setModel( "CAT" );
-truck.setColor( "blue" );
- 
-console.log( truck );
- 
-// Outputs:
-// vehicle:truck, model:CAT, color: blue
- 
-// Demonstrate "vehicle" is still unaltered
-var secondInstance = new vehicle( "car" );
-console.log( secondInstance );
- 
-// Outputs:
-// vehicle: car, model:default, license: 00000-000
+
+superHuman.setSpeed(10);
+superHuman.setPower(9000);
+
+console.log(superHuman);
+
+// Create another instance of a normal human.
+var anotherBirth = new person("human");
+console.log(anotherBirth);
--- patterns/facade.js
+++ patterns/facade.js
@@ -1,33 +1,36 @@
-var module = (function() {
- 
-    var _private = {
-        i:5,
-        get : function() {
-            console.log( "current value:" + this.i);
-        },
-        set : function( val ) {
-            this.i = val;
-        },
-        run : function() {
-            console.log( "running" );
-        },
-        jump: function(){
-            console.log( "jumping" );
-        }
-    };
- 
-    return {
- 
-        facade : function( args ) {
-            _private.set(args.val);
-            _private.get();
-            if ( args.run ) {
-                _private.run();
-            }
-        }
-    };
+var module = (function () {
+
+  var _private = {
+      price: 9000,
+      orderParts: function () {
+          console.log("Cost:" + this.price);
+      },
+      shipParts: function (cost) {
+          this.price = cost;
+      },
+      deliverParts: function () {
+          console.log("Parts Delivered.");
+      },
+      moreComplexThingsThatTheClientNeedNotKnowAbout: function () {
+          console.log("Huge facade.");
+      }
+  };
+
+  return {
+
+      facade: function (args) {
+          _private.shipParts(args.cost);
+          _private.orderParts();
+          if (args.deliverParts) {
+              _private.deliverParts();
+          }
+      }
+  };
 }());
- 
- 
-// Outputs: "current value: 10" and "running"
-module.facade( {run: true, val:10} );
+
+
+//Facade
+module.facade({
+  deliverParts: true,
+  cost: 9000
+});
