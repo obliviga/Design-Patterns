730b4e6489c361f97e26d5010ef4fca85737a87c
unknown <obliviga@gmail.com>
2014-03-23 20:03:45 -0400

    small unit test updates

---
+++ tests/creationalTest.js
@@ -0,0 +1,8 @@
+test( "Abstract test", function Toyota(model) {
+	ok( this.model = model, "Toyota created!" );
+	ok( this.model = model, "Lexus created!" );
+});
+
+test( "Builder test", function durgLab() {
+	ok( this.model = model, "Lexus created!" );
+});
