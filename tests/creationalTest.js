a965db121966e259520a259b14a2ec473a8c13d1
unknown <obliviga@gmail.com>
2014-03-24 21:04:43 -0400

    added more creational unit tests

--- tests/creationalTest.js
+++ tests/creationalTest.js
@@ -3,6 +3,18 @@ test( "Abstract test", function Toyota(model) {
 	ok( this.model = model, "Lexus created!" );
 });
 
-test( "Builder test", function durgLab() {
+test( "Builder test", function drugLab(model) {
 	ok( this.model = model, "Lexus created!" );
 });
+
+test( "Prototype test", function Laptop(type, operatingSystem, processor) {
+	ok( this.type = type, "Type prototype created!");
+	ok( this.operatingSystem = operatingSystem, "Operating System prototype created!");
+	ok( this.processor = processor, "Processor prototype created!");
+});
+
+test( "Namespace test", function car(model, lol) {
+	ok( this.model = model, "Lexus created!" );
+	ok( this.lol = lol, "Lexus created!" );
+});
+
